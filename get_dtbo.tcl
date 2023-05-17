4f43da17c25a98b24386d2e7838e8ad9237c757f
Nishant <nishantpani95@gmail.com>
2023-05-17 14:02:15 -0500

    Minimizing the generated files in the repo

---
+++ get_dtbo.tcl
@@ -0,0 +1,8 @@
+hsi open_hw_design main.xsa
+hsi set_repo_path /home/nishant/tools/Xilinx/Vivado/2022.1/KR260_Port/device-tree-xlnx
+hsi create_sw_design device-tree -os device_tree -proc psu_cortexa53_0
+hsi set_property CONFIG.dt_overlay true [hsi::get_os]
+hsi generate_target -dir bram_dts
+hsi close_hw_design main
+
+exit
